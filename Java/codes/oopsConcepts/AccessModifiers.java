2bdb985ac8e61a274ce4b7a28411cf112aa88137
salunkhegr1712 <salunkhegr1712@gmail.com>
2025-07-06 23:20:23 +0530

    feat: add AccessModifiers class and documentation on access modifiers in Java

---
+++ Java/codes/oopsConcepts/AccessModifiers.java
@@ -0,0 +1,6 @@
+package oopsConcepts;
+
+// the public which you see behind the class name is nothing but access modifier
+// access modifier to used to specify the usage of class, attribute and functions outside the class
+public class AccessModifiers {
+}
