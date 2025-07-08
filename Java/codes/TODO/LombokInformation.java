94031bd36b931d17f45468cf963ed7781702fff1
salunkhegr1712 <salunkhegr1712@gmail.com>
2025-07-08 22:10:25 +0530

    feat: update .gitignore to include .vscode and add main method to LombokInformation class

--- Java/codes/TODO/LombokInformation.java
+++ Java/codes/TODO/LombokInformation.java
@@ -2,4 +2,8 @@ package TODO;
 //::TODO : i want to learn everything related to lombok annotation
 
 public class LombokInformation {
+    public static void main(String[] args) {
+        
+        System.out.println("hello world");
+    }
 }
